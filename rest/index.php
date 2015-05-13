6a197cd9b7afe52e9f28788dfc7dc7dc9e86516f
Piotr Podstawski <piotr.podstawski@gammanet.pl>
2015-05-13 09:59:27 +0200

    no-backend

--- rest/index.php
+++ rest/index.php
@@ -3,5 +3,5 @@
     include __DIR__.'/library/backend/include/all.php';
     
     allow_origin('webkameleon.com');
-    autoload([__DIR__.'/classes',__DIR__.'/models',__DIR__.'/controllers'])
+    autoload([__DIR__.'/classes',__DIR__.'/models',__DIR__.'/controllers']);
     mydie(we);
