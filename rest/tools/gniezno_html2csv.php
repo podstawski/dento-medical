a2025cf8230e9dab3adebdff54a890fd6c7898f1
Piotr Podstawski <piotr.podstawski@gammanet.pl>
2015-05-17 09:14:59 +0200

    gn

--- rest/tools/gniezno_html2csv.php
+++ rest/tools/gniezno_html2csv.php
@@ -14,10 +14,23 @@
     function find_on_tag($html,$tags)
     {
         $name='';
+
+        $tags=strtolower($tags);
+        $html=preg_replace("/[\r\n\t ]+/",' ',$html);
         foreach (explode('|',$tags) AS $tag) {
-            $pos=strpos($html,$tag);
+            $pos=strpos(strtolower($html),$tag);
             if (!$pos) continue;
             $name=substr($html,$pos+strlen($tag));
+            
+                $t=false;
+                while ($t || $name[0]=='<' || $name[0]==' ')
+                {
+                    if ($name[0]=='<') $t=true;
+                    if ($name[0]=='>') $t=false;
+                    $name=substr($name,1);
+                }            
+            
+            
             $end=strpos($name,'<');
             $endtag=strpos($name,'>');
             if ($endtag && $endtag<$end)
@@ -33,6 +46,20 @@
         return $name;
     }
     
+    $match_array=['name'=>'<span class="bold1a"><strong>|<span class="bold1a"><strong>|<p style="text-align: justify;"><strong>|<p style="text-align: justify;">|<p><strong>|<strong><span class="bold1a">|<p align="left"><strong>',
+                  'address'=>'<td width="100%"><b><font size="2"><b>|<td width="100%"><b><font size="2">',
+                  'www'=>'www: <a href="',
+                  'email'=>'e-mail: <a href="',
+                  'phone'=>'<b>Tel.:</b>',
+                  'rector'=>'Proboszcz|Administrator|proboszcz',
+                  'sun'=>'iedziele:|niedziele i  święta:|niedziele i uroczystości:|niedziela:|niedziele i  święta:|niedziele i&nbsp; święta:',
+                  'week'=>'dni powszednie:',
+                  'fest'=>'święta zniesione:',
+                  'url'=>'xxxxxx',
+    ];
+    
+    echo '"'.implode('","',array_keys($match_array)).'"'."\n";
+    
     foreach (['b','c','d','g','i','j','k','l','l1','m','n','o','p','r','s','s1','t','u','w','z','z1'] AS $litera)
     {
         $html=url_get('http://www.archidiecezja.pl/parafie/'.$litera.'.html');
@@ -45,45 +72,28 @@
         preg_match_all('/href="([^"]+)"/',$html,$a);
         foreach ($a[1] AS $url)
         {
-            echo "Getting $url\n";
+            
             $html=url_get($url);
+            $pos=strpos($html,'<div class="catalogue_desc">');
+            if (!$pos) {
+                echo "Problem catalogue_desc: $url\n";
+                continue;
+            }
+            $html=substr($html,$pos);
+            
+            $rec=[];
+            foreach ($match_array AS $k=>$v) $rec[$k]=find_on_tag($html,$v);
+            $rec['url']=$url;
+            
+            if (!$rec['name']) continue;
+            
+            if (!$rec['sun']) echo "Brak mszy: $url\n";
+            //echo '"'.implode('","',$rec).'"'."\n";
+            
+           
+
         }
     
     }
 
-    die();
-    
-    
-    $html=file_get_contents('poznan.html');
-    
-    $mathes='';
-    
-    preg_match_all('~task,view/contact_id,([0-9]+)/~',$html,$mathes);
-    
-    $match_array=['name'=>'<h2>',
-                  'address'=>'<td width="100%"><b><font size="2"><b>|<td width="100%"><b><font size="2">',
-                  'www'=>'www: <a href="',
-                  'email'=>'e-mail: <a href="',
-                  'phone'=>'<b>Tel.:</b>',
-                  'rector'=>'<b>Proboszcz:</b>',
-                  'sun'=>'<b>Msze św. w niedziele:</b>|<b>Msze św. w niedziele: </b>|<b>Msze św. niedzielne:</b>',
-                  'week'=>'<b>Msze św. w dni powszednie:</b>',
-                  'fest'=>'<b>Msze św. w święta i w dni pracy:</b>|<b>Msze św. w święta w dni pracy:</b>|<b>Msze św. w święta w dni pracy: </b>',
-                  'url'=>'xxxxxx',
-    ];
-    
-    
-    if (isset($argv[1])) $mathes[1]=[$argv[1]];
-    
-    echo '"'.implode('","',array_keys($match_array)).'"'."\n";
-    
-    foreach ($mathes[1] AS $id) {
-        $url='http://www.archpoznan.pl/index2.php?option=com_contact&task=view&contact_id='.$id;
-        $html=url_get($url);
-        
-        $rec=[];
-        foreach ($match_array AS $k=>$v) $rec[$k]=find_on_tag($html,$v);
-        $rec['url']=$url;
-        
-        echo '"'.implode('","',$rec).'"'."\n";
-    }
+  
