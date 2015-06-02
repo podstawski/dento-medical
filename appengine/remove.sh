9217640a2088a60c333312f32b04e65fa855b64a
Piotr Podstawski <piotr.podstawski@gammanet.pl>
2015-06-02 17:23:27 +0200

    deploy

--- appengine/remove.sh
+++ appengine/remove.sh
@@ -3,4 +3,5 @@ cd `dirname $0`/tmp
 rm -rf js/pickadate.js/lib/themes-source
 rm -rf js/pickadate.js/*.md
 rm -rf js/pickadate.js/lib/translations/*.md
+rm -rf rest/tools
 find . -name .cache | xargs rm -rf
