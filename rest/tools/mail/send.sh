4bac6fb59423965f672d8b5117a8ab74387a466b
Piotr Podstawski <piotr.podstawski@gammanet.pl>
2015-06-11 20:40:40 +0200

    css

---
+++ rest/tools/mail/send.sh
@@ -0,0 +1,15 @@
+if [ ! "$2" ]
+then
+	echo "$0 adresat url-kosciola"
+	exit
+fi
+
+cd `dirname $0`
+
+sed "s/TO/$1/g" <header >x
+sed "s/URL/$2/g" <body | php -r "echo chunk_split(base64_encode(file_get_contents('php://stdin')));" >>x
+
+#sudo hostname kiedymsza.pl
+sudo sendmail -t -f piotr.podstawski@kiedymsza.pl <x
+
+#sudo hostname vps105410
--- rest/tools/send.sh
+++
@@ -1,5 +0,0 @@
-sudo hostname kiedymsza.pl
-sendmail -t <$1
-
-sleep 5
-sudo hostname vps105410
