e3dbbb7764e005838068ae469aa084775c480d0d
Piotr Podstawski <piotr@webkameleon.com>
2015-10-22 10:29:32 +0000

    ok

--- appengine/remove.sh
+++ appengine/remove.sh
@@ -1,7 +1,8 @@
 
 cd `dirname $0`/tmp
 rm -rf js/pickadate.js/lib/themes-source
-rm -rf js/pickadate.js/*.md
-rm -rf js/pickadate.js/lib/translations/*.md
+rm -f js/pickadate.js/*.md
+rm -f js/pickadate.js/lib/translations/*.md
+rm -f js/lazygrid/README.md
 rm -rf rest/tools
 find . -name .cache | xargs rm -rf
