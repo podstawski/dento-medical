a3d6a897f21b617339ca043667b0474165b8b794
Piotr Podstawski <piotr@webkameleon.com>
2017-02-07 08:13:21 +0000

    sitemap

--- sitemap.php
+++ sitemap.php
@@ -24,18 +24,14 @@
   header('Content-type: application/xml; charset=utf-8');
   
   
-?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">  
+?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">  
 <url>
     <loc>https://www.kiedymsza.pl/</loc>
-    <lastmod><?php echo sitemap_date(strtotime('2015-05-20'))?></lastmod>
-    <priority>1</priority>
 </url>
 
 <?php foreach($churches AS $ch): ?>
   <url>
     <loc>https://www.kiedymsza.pl/kosciol/<?php echo Tools::str_to_url($ch['name'])?>,<?php echo $ch['id'];?></loc>
-    <lastmod><?php echo sitemap_date($ch['change_time']>$time_change?$ch['change_time']:$time_change)?></lastmod>
-    <priority>0.9</priority>
   </url>
 <?php endforeach; ?>
 </urlset>
