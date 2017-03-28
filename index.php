7754266208ef9efc093535c1c1bff977e1ada4f5
Piotr Podstawski <piotr@webkameleon.com>
2017-03-28 17:49:50 +0000

    powrot do starego sqla

--- index.php
+++ index.php
@@ -124,15 +124,17 @@
 </div>
 <div class="col-md-3 hidden-sm hidden-xs">
   
-  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-  <!-- Homepage right -->
-  <ins class="adsbygoogle"
-       style="display:inline-block;width:100%;height:600px"
-       data-ad-client="ca-pub-3681218186493233"
-       data-ad-slot="3289357103"></ins>
-  <script>
-  (adsbygoogle = window.adsbygoogle || []).push({});
-  </script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Konkurs -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3681218186493233"
+     data-ad-slot="8609440708"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+  
 </div>
 </div>
 <div id="kiedymsza_results_template" style="display:none">
