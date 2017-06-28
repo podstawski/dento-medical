5bfba3c861d47fddd0e8f53548d9d38ee56840f0
Piotr Podstawski <piotr@webkameleon.com>
2017-06-28 16:35:57 +0000

    fb-name

--- rest/controllers/userController.php
+++ rest/controllers/userController.php
@@ -9,135 +9,138 @@ class userController extends Controller {
      */    
     protected function user($user=null)
     {
-	if (is_null($this->_user)) $this->_user=new userModel();
-	if (!is_null($user)) $this->_user->get($user);
-	return $this->_user;
+		if (is_null($this->_user)) $this->_user=new userModel();
+		if (!is_null($user)) $this->_user->get($user);
+		return $this->_user;
     }
     
     
     public function get_facebook()
     {
 	
-	$config=Bootstrap::$main->getConfig();
-	$scope="email,public_profile";
-	
-	if (Bootstrap::$main->session('fb_likes')) $scope.=",user_likes";
-	$this->check_input();
+		$config=Bootstrap::$main->getConfig();
+		$scope="email,public_profile,user_about_me";
+		
+		if (Bootstrap::$main->session('fb_likes')) $scope.=",user_likes";
+		$this->check_input();
 	
         $uri = $config['protocol'].'://' . $_SERVER['HTTP_HOST'] . Bootstrap::$main->getRoot() . 'user/facebook';
         
-	if ($this->_getParam('redirect')) Bootstrap::$main->session('auth_redirect',$this->_getParam('redirect'));
-	elseif (!Bootstrap::$main->session('auth_redirect')) mydie('redirect parameter missing','error');
+		if ($this->_getParam('redirect')) Bootstrap::$main->session('auth_redirect',$this->_getParam('redirect'));
+		elseif (!Bootstrap::$main->session('auth_redirect')) mydie('redirect parameter missing','error');
 
 	
-	if (isset($_GET['state']) && $_GET['state']==Bootstrap::$main->session('oauth2_state'))
-	{
-            if (isset($_GET['code']))
-            {
-		$url='https://graph.facebook.com/oauth/access_token';
-		$url.='?client_id='.$config['fb.app_id'];
-		$url.='&redirect_uri='.urlencode($uri);
-		$url.='&client_secret='.$config['fb.app_secret'];
-		$url.='&code='.urlencode($_GET['code']);
-		
-
-		//parse_str($this->req($url),$token);
-		$token=json_decode($this->req($url),true);
-
-
-                if (isset($token['access_token']))
-                {
-		    Bootstrap::$main->session('access_token',$token['access_token']);
-		    $auth = @json_decode(file_get_contents('https://graph.facebook.com/v2.9/me?format=json&access_token='.$token['access_token']),true);
-                    $picture = @json_decode(file_get_contents('https://graph.facebook.com/v2.9/me/picture?redirect=false&type=normal&access_token='.$token['access_token']),true);
-                 
-	    
-                    if (isset($auth['id']))
-                    {
-			$md5hash='fb.'.$auth['id'];
-			$email=isset($auth['email'])?$this->standarize_email($auth['email'],false):$md5hash;
-			$user=$this->user()->find_one_by_md5hash($md5hash);
-			
-			if (!$user)
-			{
-			    $user=$this->add(array(
-				'firstname'=>$auth['first_name'],
-				'lastname'=>$auth['last_name'],
-				'md5hash'=>$md5hash,
-				'email'=>$email
-			    ),false);
-			}
-			
-			$model=new userModel($user['id']);
-			
-			if (!$model->firstname) $model->firstname = $auth['first_name'];
-			if (!$model->lastname) $model->lastname = $auth['last_name'];
-			if (isset($picture['data']['url']))
-			    if (!$model->photo || strstr($model->photo,'fbcdn'))
-				$model->photo = $picture['data']['url'];
+		if (isset($_GET['state']) && $_GET['state']==Bootstrap::$main->session('oauth2_state'))
+		{
+				if (isset($_GET['code']))
+				{
+					$url='https://graph.facebook.com/oauth/access_token';
+					$url.='?client_id='.$config['fb.app_id'];
+					$url.='&redirect_uri='.urlencode($uri);
+					$url.='&client_secret='.$config['fb.app_secret'];
+					$url.='&code='.urlencode($_GET['code']);
+					
+	
+					//parse_str($this->req($url),$token);
+					$token=json_decode($this->req($url),true);
+	
+	
+					if (isset($token['access_token']))
+					{
+						Bootstrap::$main->session('access_token',$token['access_token']);
+						$auth = @json_decode(file_get_contents('https://graph.facebook.com/v2.9/me?fields=id,email,name&format=json&access_token='.$token['access_token']),true);
+						$picture = @json_decode(file_get_contents('https://graph.facebook.com/v2.9/me/picture?redirect=false&type=normal&access_token='.$token['access_token']),true);
+					 
+						
+						if (isset($auth['id']))
+						{
+							$md5hash='fb.'.$auth['id'];
+							$email=isset($auth['email'])?$this->standarize_email($auth['email'],false):$md5hash;
+							$user=$this->user()->find_one_by_md5hash($md5hash);
+							$fbname=$auth['name'].explode(' ');
+							
+							if (!$user)
+							{
+								$user=$this->add(array(
+								'firstname'=>$fbname[0],
+								'lastname'=>$fbname[1],
+								'md5hash'=>$md5hash,
+								'email'=>$email
+								),false);
+							}
 				
+							$model=new userModel($user['id']);
+							
+							if (!$model->firstname) $model->firstname = $fbname[0];
+							if (!$model->lastname) $model->lastname = $fbname[1];
+							if(strstr($email,'@')) $model->email=$email;
+							
+							if (isset($picture['data']['url']))
+								if (!$model->photo || strstr($model->photo,'fbcdn'))
+									$model->photo = $picture['data']['url'];
+					
+				
+							if (!$model->url) $model->url='https://www.facebook.com/'.$auth['id'];
+				
+							$model->save();
+				
+							$data=$model->data();
+				
+							Tools::log('fb-login',['get'=>$_GET,'token'=>$token,'auth'=>$auth, 'user'=>$data]);
+							
+							if(isset($data['password'])) unset($data['password']);
+							Bootstrap::$main->session('user',$data);
+							Bootstrap::$main->session('auth', $auth);
+							
+							$this->redirect(Bootstrap::$main->session('auth_redirect'));
+							
+						}
+						else
+						{
+							if (isset($auth['error']))
+							{
+								Bootstrap::$main->session('error', $auth['error']['message']);
+							}
+							Tools::log('fb-login-error',['get'=>$_GET,'token'=>$token,'auth'=>$auth]);
+							$this->redirect(Bootstrap::$main->session('auth_redirect'));
+						}
+						
+					}               
+					else
+					{
+						Tools::log('fb-login-error',['get'=>$_GET,'token'=>$token]);
+						$this->redirect(Bootstrap::$main->session('auth_redirect'));
+					}
+					
+				} else {
+					$this->redirect(Bootstrap::$main->session('auth_redirect'));
+				}
+			} elseif (isset($_GET['state'])) {
+				Tools::log('fb-login-error',['get'=>$_GET]);
+				$this->redirect($uri);	    
+			} else {
 			
-			if (!$model->url) $model->url='https://www.facebook.com/'.$auth['id'];
+				$state=md5(rand(90000,1000000).time());
+				Bootstrap::$main->session('oauth2_state',$state);
 			
-			$model->save();
 			
-			$data=$model->data();
-			
-			Tools::log('fb-login',['get'=>$_GET,'token'=>$token,'auth'=>$auth, 'user'=>$data]);
-			
-			if(isset($data['password'])) unset($data['password']);
-			Bootstrap::$main->session('user',$data);
-                        Bootstrap::$main->session('auth', $auth);
-			
-			$this->redirect(Bootstrap::$main->session('auth_redirect'));
-                        
-                    }
-                    else
-                    {
-                        if (isset($auth['error']))
-                        {
-			    Bootstrap::$main->session('error', $auth['error']['message']);
-                        }
-			Tools::log('fb-login-error',['get'=>$_GET,'token'=>$token,'auth'=>$auth]);
-                        $this->redirect(Bootstrap::$main->session('auth_redirect'));
-                    }
-                    
-                }               
-                else
-                {
-		    Tools::log('fb-login-error',['get'=>$_GET,'token'=>$token]);
-                    $this->redirect(Bootstrap::$main->session('auth_redirect'));
-                }
-                
-            } else {
-                $this->redirect(Bootstrap::$main->session('auth_redirect'));
-            }
-        } elseif (isset($_GET['state'])) {
-	    Tools::log('fb-login-error',['get'=>$_GET]);
-            $this->redirect($uri);	    
-	} else {
-	    
-            $state=md5(rand(90000,1000000).time());
-            Bootstrap::$main->session('oauth2_state',$state);
-	    
-	    
-	    $url='https://www.facebook.com/v2.9/dialog/oauth';
-	    $url.='?client_id='.$config['fb.app_id'];
-	    $url.='&redirect_uri='.urlencode($uri);
-	    $url.='&state='.$state;
-	    $url.='&scope='.urlencode($scope);
-	    
-	    $this->redirect($url);
-	}
-	
+				$url='https://www.facebook.com/v2.9/dialog/oauth';
+				$url.='?client_id='.$config['fb.app_id'];
+				$url.='&redirect_uri='.urlencode($uri);
+				$url.='&state='.$state;
+				$url.='&scope='.urlencode($scope);
+				
+				$this->redirect($url);
+			}
+		
     }
     
     
     public function get_logout()
     {
-	Bootstrap::$main->session('user',false);
-	Bootstrap::$main->logout();
-	return $this->status();
+		Bootstrap::$main->session('user',false);
+		Bootstrap::$main->logout();
+		return $this->status();
     }
 
     protected function standarize_email($email,$error=true)
