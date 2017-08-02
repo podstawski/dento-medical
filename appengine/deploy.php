1be9885249122f9353d4dac3eb9c25ebde5b0840
Piotr Podstawski <piotr@webkameleon.com>
2017-08-02 19:15:06 +0000

    moja

--- appengine/deploy.php
+++ appengine/deploy.php
@@ -26,7 +26,7 @@
         $files[]=$f;
         system($cmd);
         
-	if ($f=='admin' || $f=='rest' || $f=='kosciol' ||$f=='edit' ||$f=='html' || $f=='o-projekcie' || $f=='mapa' || $f=='konkurs' || $f=='dbg') continue;
+	if ($f=='admin' || $f=='rest' || $f=='kosciol' ||$f=='edit' ||$f=='html' || $f=='o-projekcie' || $f=='mapa' || $f=='konkurs' || $f=='dbg' || $f=='moja') continue;
 
 	
         if (is_dir("$dir/$f"))
