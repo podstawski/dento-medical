577a18fd1dbf5769f8687d21df07c906204790a8
Piotr Podstawski <piotr@webkameleon.com>
2019-11-26 07:39:33 +0000

    deploy

--- appengine/deploy.php
+++ appengine/deploy.php
@@ -54,6 +54,7 @@
     $dir=explode('/',__DIR__);
     file_put_contents(__DIR__.'/log.txt',date('Y-m-d H:i:s')."\n",FILE_APPEND);
     $cmd="/opt/google/appengine/appcfg.py -e $mail update $dest";
+    $cmd="cd $dest; gcloud app deploy app.yaml --project kiedymsza";
     system('git commit -m deploy '.__DIR__.' 2>/dev/null');
     system('git push origin master');
     system($cmd);
